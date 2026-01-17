e3d9aaf0d5ccbd5a2a8f71123e2ea3f27ac86309
Simon Hartcher <simon@simonhartcher.com>
2026-01-17 14:21:43 +1100

    test(resources): add cloud sync credentials metadata test

--- internal/resources/cloudsync_credentials_test.go
+++ internal/resources/cloudsync_credentials_test.go
@@ -1,6 +1,7 @@
 package resources
 
 import (
+	"context"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -15,4 +16,19 @@ func TestNewCloudSyncCredentialsResource(t *testing.T) {
 	var _ resource.Resource = r
 	var _ resource.ResourceWithConfigure = r.(*CloudSyncCredentialsResource)
 	var _ resource.ResourceWithImportState = r.(*CloudSyncCredentialsResource)
+}
+
+func TestCloudSyncCredentialsResource_Metadata(t *testing.T) {
+	r := NewCloudSyncCredentialsResource()
+
+	req := resource.MetadataRequest{
+		ProviderTypeName: "truenas",
+	}
+	resp := &resource.MetadataResponse{}
+
+	r.Metadata(context.Background(), req, resp)
+
+	if resp.TypeName != "truenas_cloudsync_credentials" {
+		t.Errorf("expected TypeName 'truenas_cloudsync_credentials', got %q", resp.TypeName)
+	}
 }
